22d900cf16dd5a1399927b4954a218e284601766
pngwn <hello@pngwn.io>
2022-04-28 12:20:21 +0100

    update highlighted text

--- demo/diff_texts/run.py
+++ demo/diff_texts/run.py
@@ -14,10 +14,12 @@ def diff_texts(text1, text2):
 demo = gr.Interface(
     diff_texts,
     [
-        gr.Textbox(lines=3, default_value="The quick brown fox jumped over the lazy dogs."),
+        gr.Textbox(
+            lines=3, default_value="The quick brown fox jumped over the lazy dogs."
+        ),
         gr.Textbox(lines=3, default_value="The fast brown fox jumps over lazy dogs."),
     ],
-    gr.HighlightedText(),
+    gr.HighlightedText(show_legend=True),
 )
 if __name__ == "__main__":
     demo.launch()
